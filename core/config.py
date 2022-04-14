5d2ef0cef72bb76485b4a8b192d6d5beedc9ec0e
Evgenii Smolentsev <81253133+evsmol@users.noreply.github.com>
2022-04-15 01:31:03 +0500

    fix heroku

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DB_URL']
 VERSION = os.environ['VERSION']
 BOT_ID = os.environ['BOT_ID']
 BOT_TOKEN = os.environ['BOT_TOKEN']
